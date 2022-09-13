af7300c9eb985a9a89013f9ca30755a0ca617c5a
hpipou <mr.aoudjit@gmail.com>
2022-09-13 08:57:30 +0200

    Email Sender Service

--- src/main/java/io/spring/sendemail/SendemailApplication.java
+++ src/main/java/io/spring/sendemail/SendemailApplication.java
@@ -2,12 +2,25 @@ package io.spring.sendemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import javax.mail.MessagingException;
 
 @SpringBootApplication
 public class SendemailApplication {
+
+	@Autowired
+	private EmailSenderService senderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SendemailApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException {
+		senderService.sendSimpleEmail("FromEMAIL@gmail.com",
+				"Validation du compte",
+				"code d'inscription 123456789");
+	}
 }
